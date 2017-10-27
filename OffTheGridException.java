3347333b2c9b4c8734d27d704fb808a9c9e0ac00
averma246 <32584962+averma246@users.noreply.github.com>
2017-10-26 23:09:50 -0400

    Create OffTheGridException.java

---
+++ OffTheGridException.java
@@ -0,0 +1,12 @@
+/**
+ * Throwable exception that is thrown when a cell that is more than one 
+ * <code>Cell</code> away from the grid is attempted to be accessed.Contains
+ * nothing.
+ * @author Ana Verma averma20@amherst.edu
+ **/
+
+//==============================================================================
+
+//==============================================================================
+
+public class OffTheGridException extends RuntimeException{}
